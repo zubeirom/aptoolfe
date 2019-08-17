47bbac7562b5461cd27145f06c002e26b255a49a
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-17 04:46:52 +0200

    Fix errors and setup namespace

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 import ENV from "../config/environment";
 
 export default DS.JSONAPIAdapter.extend({
-    host: ENV.host
+    host: ENV.host,
+    namespace: 'api'
 });
