039875c679518b58afc185f19ea3f29a3ef54ee5
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-17 18:36:35 +0200

    Setup design and fonts:wq

---
+++ app/controllers/sign-up.js
@@ -0,0 +1,43 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
+
+export default Controller.extend({
+    toastr: service('toast'),
+    session: service(),
+
+    checklist(obj) {
+        if (obj.username !== undefined && obj.firstname !== undefined && obj.lastname !== undefined && obj.password !== undefined) {
+            return true
+        }
+        this.toastr.error('Please enter all fields', 'Warning');
+        return false
+    },
+
+    actions: {
+
+        async register() {
+            if (this.checklist(this.model)) {
+                if (this.model.password === this.secondPassword) {
+                    try {
+                        set(this, 'secondPassword', "");
+                        await this.model.save();
+                        let { user_name, password } = this.model;
+                        this.get('session').authenticate('authenticator:jwt', user_name, password).catch((reason) => {
+                            this.set('errorMessage', reason.error || reason);
+                        });
+                    } catch (error) {
+                        console.log(error);
+                        this.toastr.error('Account exists already', 'Error')
+                    }
+                } else {
+                    this.toastr.warning("Passwords don't match", 'Warning')
+                }
+            }
+        },
+
+        redirect() {
+            this.send('register')
+        }
+    }
+});
--- app/models/account.js
+++ app/models/account.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    username: DS.attr('string'),
+    firstname: DS.attr('string'),
+    lastname: DS.attr('string'),
+    email: DS.attr('string'),
+    tel: DS.attr('string'),
+    password: DS.attr('string'),
+    statuses: DS.attr(),
+    socials: DS.attr(),
+    jobKeywords: DS.attr()
 });
