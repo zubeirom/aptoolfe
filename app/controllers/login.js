96afcdb51e603c6f775ecefc60e02cb72c61aeb4
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-18 18:00:45 +0200

    Add validation for input in login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -5,13 +5,25 @@ export default Controller.extend({
     toastr: service('toast'),
     session: service(),
 
+    isValid(username, password) {
+        if (username === undefined || username === "" && password === undefined || password === "") {
+            this.toastr.warning('Please enter something', 'Warning');
+            return false
+        }
+        return true
+    },
+
     actions: {
         authenticate() {
             try {
                 let { username, password } = this.getProperties('username', 'password');
-                this.get('session').authenticate('authenticator:oauth2', username, password).catch((reason) => {
-                    this.set('errorMessage', reason.error || reason);
-                });
+                if (this.isValid(username, password)) {
+                    this.get('session').authenticate('authenticator:oauth2', username, password).catch((reason) => {
+                        this.set('errorMessage', reason.error || reason);
+                        this.toastr.error('Password or username is wrong', 'Error');
+                    });
+
+                }
             } catch (error) {
                 if (error) {
                     this.toastr.error('Password or username is wrong', 'Error');
