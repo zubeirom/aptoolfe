106acb7b4f738773820782f3bf3121758b7ac2a1
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-20 23:23:23 +0200

    modify application model

--- app/models/application.js
+++ app/models/application.js
@@ -2,11 +2,11 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     account: DS.belongsTo(),
-    company: DS.belongsTo(),
+    company: DS.attr(),
     created: DS.attr(),
     status: DS.attr(),
     submissionType: DS.attr(),
     source: DS.attr(),
     recruiters: DS.attr(),
-    position: DS.belongsTo()
+    occupation: DS.attr()
 });
