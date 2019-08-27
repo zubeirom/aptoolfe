6e53ea63be3a0573ecdb7b753678e2c17f791946
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-27 02:38:53 +0200

    Add delete route for event

--- app/controllers/applications/info/event/info/index.js
+++ app/controllers/applications/info/event/info/index.js
@@ -1,4 +1,15 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    actions: {
+        delete() {
+            try {
+                this.model.destroyRecord();
+                this.transitionToRoute('applications.info', this.model.application)
+                this.model.application.events.removeObject(this.model)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
 });
