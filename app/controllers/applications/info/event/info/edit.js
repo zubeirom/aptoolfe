e6f9cdf155b3a1b428e50493c021eff98ac4b062
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-27 02:17:19 +0200

    Add edit event template and update event

--- app/controllers/applications/info/event/info/edit.js
+++ app/controllers/applications/info/event/info/edit.js
@@ -1,4 +1,21 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+    toastr: service('toast'),
+
+    actions: {
+        update() {
+            if (this.model.name && this.model.date) {
+                try {
+                    this.model.save();
+                    this.transitionToRoute('applications.info.event.info', this.model)
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                this.toastr.error('Please enter the name and the date of the event', 'Error');
+            }
+        }
+    }
 });
