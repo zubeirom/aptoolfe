1567d0d239bcce4d8a46e16d5b06ff45af6b2bf2
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-28 04:34:50 +0200

    PATCH account

--- app/controllers/my-account.js
+++ app/controllers/my-account.js
@@ -1,9 +1,25 @@
 import Controller from '@ember/controller';
+import { set } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+    toastr: service('toast'),
+
     actions: {
-        pushKeyword(keyword) {
-            this.model.jobKeywords.pushObject(keyword);
-       }
+        addKeyword(keyword) {
+            this.model.job_keywords.pushObject(keyword);
+            set(this, 'keyword', '');
+        },
+        deleteKeyword(keyword) {
+            this.model.job_keywords.removeObject(keyword);
+        },
+        async saveChanges() {
+            try {
+                await this.model.save();
+                this.toastr.success('Successfully changed account', 'Nice!');
+            } catch (error) {
+                console.log(error)
+            }
+        }
    } 
 });
