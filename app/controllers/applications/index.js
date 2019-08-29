38f8af3748eeabfce944a4708ea0f61cc58f8bee
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-29 02:59:23 +0200

    Add dropdown for query

--- app/controllers/applications/index.js
+++ app/controllers/applications/index.js
@@ -1,4 +1,8 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    init() {
+        this._super(...arguments);
+        this.types = ['Status', 'CompanyName']
+    }
 });
