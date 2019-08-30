8e8f071b8a6296f63ffdac545f26da3821f0b2ad
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-31 01:40:41 +0200

    remove route access check, implemented in server

--- app/routes/applications/info.js
+++ app/routes/applications/info.js
@@ -4,27 +4,4 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
     ajax: service(),
     session: service(),
-
-    async afterModel(model) {
-        try {
-            const res = await this.ajax.request('http://localhost:3000/api/routeAccessCheck', {
-                method: 'POST',
-                data: {
-                    id: model.id,
-                    accessToken: this.session.data.authenticated.access_token
-                }
-            });
-            console.log(res);
-            if (res === 'Unauthorized') {
-                this.transitionTo('applications');
-                window.location = '/applications';
-            }
-        } catch (error) {
-            console.log(error);
-            if (error === 'Unauthorized') {
-                this.transitionTo('applications');
-                window.location = '/applications';
-            }
-        }
-    }
 });
