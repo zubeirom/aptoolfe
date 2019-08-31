0305d2d3fa0151bb0eacf15e09a2f2a8c5d60936
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-31 03:45:47 +0200

    8 characters password requirement

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -19,21 +19,25 @@ export default Controller.extend({
         async register() {
             if (this.checklist(this.model)) {
                 if (this.model.password === this.secondPassword) {
-                    try {
-                        await this.model.save();
-                        const { username } = this.model;
-                        this.get('session').authenticate('authenticator:oauth2', username, this.secondPassword).catch((reason) => {
-                            this.set('errorMessage', reason.error || reason);
-                            console.log(reason.error || reason)
-                        });
-                        set(this, 'secondPassword', '');
-                    } catch (error) {
-                        console.log(error);
-                        this.toastr.error('Account exists already', 'Error');
-                        this.transitionToRoute('sign-up')
+                    if (this.model.password.length < 8) {
+                        this.toastr.warning('Password must have 8 characters', 'Warning');
+                    } else {
+                        try {
+                            await this.model.save();
+                            const { username } = this.model;
+                            this.get('session').authenticate('authenticator:oauth2', username, this.secondPassword).catch((reason) => {
+                                this.set('errorMessage', reason.error || reason);
+                                console.log(reason.error || reason)
+                            });
+                            set(this, 'secondPassword', '');
+                        } catch (error) {
+                            console.log(error);
+                            this.toastr.error('Account exists already', 'Error');
+                            this.transitionToRoute('sign-up')
+                        }
                     }
                 } else {
-                    this.toastr.warning('Passwords don\'t match', 'Warning')
+                    this.toastr.warning('Passwords don\'t match', 'Warning');
                 }
             }
         },
