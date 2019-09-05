c9279681361c7c29f7441de7b546a55549ece741
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-09-05 02:10:36 +0200

    Add ability to add applications to list

--- app/components/job-segment.js
+++ app/components/job-segment.js
@@ -1,4 +1,25 @@
 import Component from '@ember/component';
+import { set, get } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+    store: service(),
+
+    actions: {
+        async addApplication(model, title, company) {
+            try {
+                set(this, 'loader', true);
+                const saveApplication = await get(this, 'store').createRecord('application', {
+                    account_id: model,
+                    company,
+                    occupation: title
+                });
+                await saveApplication.save();
+                set(this, 'loader', false);
+                set(this, 'saved', true);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
 });
